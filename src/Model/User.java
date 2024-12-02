e7852bebaec0d7cd442ea1699af978428599f6da
Lê Bá Thành Trung <dorakido72@gmail.com>
2024-12-02 22:00:31 +0700

    a

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,24 +1,16 @@
 package Model;
 
 public class User {
-    private String userID, userName, password, role;
-    private static int id = 1;
+    private String userName, password, role;
 
     public User(){
 
     }
-    public User( String userName, String password, String role ) {
-        this.userID = String.format("U%04d", id++);
+
+    public User(String userName, String password, String role ) {
         this.userName = userName;
         this.password = password;
         this.role = role;
-    }
-    public String getUserID() {
-        return userID;
-    }
-
-    public void setUserID(String userID) {
-        this.userID = userID;
     }
 
     public String getUserName() {
