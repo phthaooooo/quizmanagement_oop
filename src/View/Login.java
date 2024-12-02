3e44aab41a672ba8d54f921eed2a1697ee2578b8
nghieu19 <hieu190904@gmail.com>
2024-12-03 00:40:12 +0700

    Update Login.java

--- src/View/Login.java
+++ src/View/Login.java
@@ -18,19 +18,19 @@ public class Login extends JFrame implements ActionListener {
     private static final String username_admin = "admin";
     private static final String password_admin = "admin";
     Login() {
-        getContentPane().setBackground(new Color(0, 51, 102));
+        getContentPane().setBackground(new Color(245, 245, 245));
         setLayout(null);
         // heading name
-        JLabel heading = new JLabel("LOGIN");
-        heading.setBounds(380, 60, 300, 45);
-        heading.setFont(new Font("SansSerif", Font.BOLD + Font.ITALIC, 40));
-        heading.setForeground(new Color(30, 144, 254));
+        JLabel heading = new JLabel("LOG - IN");
+        heading.setBounds(360, 70, 300, 45);
+        heading.setFont(new Font("SansSerif", Font.BOLD + Font.PLAIN, 40));
+        heading.setForeground(new Color(0, 0, 0));
         add(heading);
         //username
         JLabel username = new JLabel("username");
         username.setBounds(200, 150, 300, 20);
         username.setFont(new Font("Arial", Font.BOLD, 18));
-        username.setForeground(new Color(30, 144, 254));
+        username.setForeground(new Color(0, 0, 0));
         add(username);
 
         tfusername = new JTextField();
@@ -41,7 +41,7 @@ public class Login extends JFrame implements ActionListener {
         JLabel pw = new JLabel("password");
         pw.setBounds(200, 200, 300, 20);
         pw.setFont(new Font("Arial", Font.BOLD, 18));
-        pw.setForeground(new Color(30, 144, 254));
+        pw.setForeground(new Color(0, 0, 0));
         add(pw);
 
         tfpassword = new JPasswordField();
@@ -51,28 +51,24 @@ public class Login extends JFrame implements ActionListener {
 
         // Button
         back = new JButton("Back");
-        back .setBounds(200, 300, 120, 30);
-        back .setBackground(new Color(30, 144, 254));
-        back .setForeground(Color.WHITE);
-        back .setFont(new Font("Arial", Font.BOLD, 16)); // Chọn font Arial, kiểu chữ thường, cỡ 16
+        back .setBounds(300, 300, 120, 30);
+        back .setForeground(Color.BLACK);
+        back .setFont(new Font("Arial", Font.BOLD, 12)); // Chọn font Arial, kiểu chữ thường, cỡ 16
         back .addActionListener(this);
         add(back );
 
 
         next = new JButton("Next");
-        next.setBounds(600, 300, 120, 30);
-        next.setBackground(new Color(30, 144, 254));
-        next.setForeground(Color.WHITE);
-        next.setFont(new Font("Arial", Font.BOLD, 16)); // Chọn font Arial, kiểu chữ thường, cỡ 16
+        next.setBounds(500, 300, 120, 30);
+        next.setForeground(Color.BLACK);
+        next.setFont(new Font("Arial", Font.BOLD, 12)); // Chọn font Arial, kiểu chữ thường, cỡ 16
         next.addActionListener(this);
         add(next);
 
-        forgotpw = new JButton("Forgot password");
-        forgotpw.setBounds(580, 250, 150, 30);
-        forgotpw.setBackground(new Color(0, 51, 102));
-        forgotpw.setForeground(Color.WHITE);
-        forgotpw.setFont(new Font("Arial", Font.BOLD, 11)); // Chọn font Arial, kiểu chữ thường, cỡ 16
-        forgotpw.addActionListener(this);
+        forgotpw = new JButton("Forgot password"); 
+        forgotpw.setBounds(540, 240, 150, 30); 
+        forgotpw.setFont(new Font("Arial", Font.BOLD, 11)); 
+        forgotpw.setForeground(Color.BLACK); 
         add(forgotpw);
 
         setSize(1000, 500);
@@ -98,7 +94,7 @@ public class Login extends JFrame implements ActionListener {
                 if (username.equals(username_admin) && password.equals(password_admin)) {
                     var admin = new Admin(username_admin, password_admin, "admin");
                     this.dispose();
-                    new AdminUI();
+                    //new AdminUI(admin);
                 } else {
                     var loginUser = UserQuery.selectByAccount(username, password);
                     if(loginUser != null) {
